8e8ca4390648b968dda3ac59ee10d4e17e0e9964
zouxiangyue <1539442166@qq.com>
2019-04-17 16:56:37 +0800

    密码可见

---
+++ 42-password-visual/password.js
@@ -0,0 +1,28 @@
+$(function(){
+  var $addpwd=$("#addpwd"),
+      $getpwd=$("#getpwd"),
+      $show=$("#show"),
+      $imgs=$('')
+  $addpwd.click(function(){
+      var $newpwd=$('<div>密码：<input value="" type="password"/><img src="./images/eye1.png"/></div>');
+      $show.append($newpwd);
+      var $imgs=$("img");
+      var $pwd=$("input")
+      for(let j=0;j<$imgs.length;j++){
+        $imgs[j].onmouseover=function(){
+          $pwd[j].type='text'
+          $imgs[j].src="./images/eye2.png"
+        };
+        $imgs[j].onmouseout=function(){
+          $pwd[j].type='password';
+          $imgs[j].src='./images/eye1.png';
+      } 
+    }   
+  });
+  $getpwd.click(function(){
+    var $pwd=$("input")
+    for(var i=0;i<$pwd.length;i++){
+      alert($pwd[i].value);
+    }
+  })
+})
